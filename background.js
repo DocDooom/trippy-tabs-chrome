3b5dc3eda9e1a74474bf1e003ae18cbec8d4fcbf
DocDooom <kidsonictrading@gmail.com>
2023-03-03 17:59:37 +0000

    General updates

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
-// Service Worker runs when needed deactivates when it's not
-// Will lose the state when deactivated
-// To get persistent state we need to store it on users HDD
+
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.
+    chrome.storage.local.set({
+        name: "Jack"
+    });
 });
+
+// Get installed data
+chrome.storage.local.get('name', data => {});
